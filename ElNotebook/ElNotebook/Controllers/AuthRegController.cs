b30bce31316aba7ad623ec5b9b7d932574a97a36
TUNO\kikut <kikutepsilon@gmail.com>
2023-11-21 14:03:28 +0300

    added gitignore file and AuthRegController

---
+++ ElNotebook/ElNotebook/Controllers/AuthRegController.cs
@@ -0,0 +1,24 @@
+﻿using ElNotebook.Models;
+using System.Data;
+
+namespace ElNotebook.Controllers
+{
+    public class AuthRegController
+    {
+        public static User Authorize(string login, string password)
+        {
+            //return ManagerOperation.Manager.GetUsers().Where(u => u.Name == login && u.Password == password).FirstOrDefault();
+            throw new NotImplementedException(); 
+        }
+
+        public static User Register(string login, string password)
+        {
+            //if (ManagerOperation.Manager.GetUsers().Where(u => u.Name == login && u.Password == password).Count() != 0)
+            //    new DuplicateNameException();
+            //User user = new User() { Name = login, Password = password };
+            //ManagerOperation.Manager.AddUser(user);
+            //return user;
+            throw new NotImplementedException();
+        }
+    }
+}
